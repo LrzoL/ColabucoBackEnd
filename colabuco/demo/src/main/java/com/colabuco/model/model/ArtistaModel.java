1e44658cb6a85122ad445d5b4bbf99f8867160f5
iale.almeida_ <almeidaialesantos@gmail.com>
2023-12-05 23:21:07 -0300

    Adicionado o model de Artista e Carrinho de compras

--- colabuco/demo/src/main/java/com/colabuco/model/model/ArtistaModel.java
+++ colabuco/demo/src/main/java/com/colabuco/model/model/ArtistaModel.java
@@ -1,4 +1,66 @@
 package com.colabuco.model.model;
+import java.util.ArrayList;
 
-public class ArtistaModel {
+public class ArtistaModel extends ClienteModel{
+    private String cnpj;
+    private ArrayList<String> categoriaArte;
+    private String politicaEntregaFrete;
+    private String descricao;
+    private ArrayList<ProdutoModel> produtos;
+
+    public ArtistaModel(String nome, String email, String cpf, String telefone, String senha, String endereco, String cnpj, ArrayList<String> categoriaArte, String politicaEntregaFrete, String descricao){
+        super(nome, email, cpf, telefone, senha, endereco);
+        this.cnpj = cnpj;
+        categoriaArte = new ArrayList<String>();
+        this.politicaEntregaFrete = politicaEntregaFrete;
+        this.descricao = descricao;
+        this.produtos = new ArrayList<ProdutoModel>();
+    }
+
+    //Sobrecarga de construtor
+    public ArtistaModel(String nome, String email, String cpf, String telefone, String senha, String endereco, ArrayList<String> categoriaArte, String politicaEntregaFrete, String descricao){
+        super(nome, email, cpf, telefone, senha, endereco);
+        this.cnpj = null;
+        categoriaArte = new ArrayList<String>();
+        this.politicaEntregaFrete = politicaEntregaFrete;
+        this.descricao = descricao;
+        this.produtos = new ArrayList<ProdutoModel>();
+    }
+
+    //getters
+    public String getCnpjModel(){
+        return this.cnpj;
+    }
+    public ArrayList<String> getCategoriaArte(){
+        return this.categoriaArte;
+    }
+    public String getPoliticaEntregaFrete(){
+        return this.politicaEntregaFrete;
+    }
+    public String getDescricao(){
+        return this.descricao;
+    }
+    public ArrayList<ProdutoModel> getProdutos(){
+        return produtos;
+    }
+
+    //setters
+    public void setCnpj(String novoCnpj){
+        this.cnpj = novoCnpj;
+    }
+    public void setCategoriaArte (ArrayList<String> novaCategoriaArte){
+        this.categoriaArte = novaCategoriaArte;
+    }
+    public void setPoliticaEntregaFrete(String novaPoliticaEntregaFrete){
+        this.politicaEntregaFrete = novaPoliticaEntregaFrete;
+    }
+    public void setDescricao(String novaDescricao){
+        this.descricao = novaDescricao;
+    }
+
+    //métodos
+    public void cadastrarProduto(String id, String nome, ArrayList<String> categoria, String descricao, double preco, ArtistaModel artista){
+        ProdutoModel p1 = new ProdutoModel(id, nome, categoria, descricao, preco, artista);
+    }
+
 }
