7f44a80bbd24624ca5ba7c7fc6723cb3421405db
RafaFigueiroa <rafael.00000847851@unicap.br>
2023-12-11 17:50:28 -0300

    model do carrinho de compras

--- colabuco/demo/src/main/java/com/colabuco/model/model/CarrinhoDeComprasModel.java
+++ colabuco/demo/src/main/java/com/colabuco/model/model/CarrinhoDeComprasModel.java
@@ -2,13 +2,34 @@ package com.colabuco.model.model;
 import java.util.ArrayList;
 import java.util.Random;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_carrinho_de_compra_model")
 public class CarrinhoDeComprasModel {
     Random random = new Random();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+    @Column(nullable = false)
     private double valor;
+
+    @OneToMany(mappedBy = "produto", cascade = CascadeType.ALL, orphanRemoval = true)
     private ArrayList<ProdutoModel> produtos;
+
+    @Column(nullable = false)
     private String cpfCliente;
+    @Column(nullable = false)
     private PedidoModel pedido;
+    @Column(nullable = false)
     private ClienteModel cliente;
 
     //construtor
