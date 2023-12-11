1ba7bc85bb650483bccd2d619c31d7b252361cb7
Nicholas-Bergqvist <nicholas.2021150430@unicap.br>
2023-12-11 17:12:16 -0300

    extendi cliente e pedido repository

--- colabuco/demo/src/main/java/com/colabuco/model/repository/ClienteRepository.java
+++ colabuco/demo/src/main/java/com/colabuco/model/repository/ClienteRepository.java
@@ -4,8 +4,6 @@ import com.colabuco.model.model.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel,Long> {
 }
